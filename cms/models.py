f571b0f4efe37524ee00d1cc5d4197fa96f89b27
koaf <76429102+koaf@users.noreply.github.com>
2025-05-15 15:34:08 +0900

    add UserBalance

--- cms/models.py
+++ cms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 # Create your models here.
 
 # Django標準のユーザー機能からカスタムフィールドを追加
-class CustomUser(AbstractUser):
-    pass
+class CustomUser(User):
+    Balance = models.IntegerField('残高',default=1000)
